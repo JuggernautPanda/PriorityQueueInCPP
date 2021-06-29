70bc88045bfa527c3038a352c1acdd3d270bf604
Juggernaut Panda <grajasumant@ieee.org>
2021-06-29 14:42:26 +0530

    Added a low priority handler

--- priorityQueue.h
+++ priorityQueue.h
@@ -10,6 +10,7 @@ class Priority_Q
     Priority_Q();
     void insertToQueue(int priority ,string value);
     int getmaxpriority(void);
+    int getlowpriority(void);
     void deleteFromQueue(void);
     void loadInPut(void);
     void outFunc1(int ind);
